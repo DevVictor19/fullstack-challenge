4414b1edca2468bc36b9b2d2597e9ef770f7bb91
DevVictor19 <antoniovictor12@live.com>
2024-08-15 17:37:10 -0300

    feat(backend): add imports to app module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common';
+import { EnvConfigModule } from './env-config/env-config.module';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [],
+  imports: [EnvConfigModule, DatabaseModule],
 })
 export class AppModule {}
