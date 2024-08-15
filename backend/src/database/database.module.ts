7ee84a39dd35713e1cc132fcd2ddd5f28befa923
DevVictor19 <antoniovictor12@live.com>
2024-08-15 20:26:02 -0300

    feat(backend): add user entity to database

--- backend/src/database/database.module.ts
+++ backend/src/database/database.module.ts
@@ -1,21 +1,22 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { EnvConfigService } from 'src/env-config/env-config.service';
+import { IEnvConfigService } from 'src/env-config/env-config-service.interface';
+import { User } from 'src/users/users.entity';
 
 @Module({
   imports: [
     TypeOrmModule.forRootAsync({
-      useFactory: (envConfigService: EnvConfigService) => ({
+      useFactory: (envConfigService: IEnvConfigService) => ({
         type: 'postgres',
         host: envConfigService.getDatabaseHost(),
         port: envConfigService.getDatabasePort(),
         username: envConfigService.getDatabaseUser(),
         password: envConfigService.getDatabasePass(),
         database: envConfigService.getDatabaseName(),
-        entities: [],
+        entities: [User],
         synchronize: true, // only in development!
       }),
-      inject: [EnvConfigService],
+      inject: [IEnvConfigService],
     }),
   ],
 })
