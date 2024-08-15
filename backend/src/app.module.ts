039a16c67e921dcb4bcec30d50f3fca8a9d7d78c
DevVictor19 <antoniovictor12@live.com>
2024-08-15 20:26:29 -0300

    feat(backend): update imports app module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,8 +1,17 @@
 import { Module } from '@nestjs/common';
 import { EnvConfigModule } from './env-config/env-config.module';
 import { DatabaseModule } from './database/database.module';
+import { JwtModule } from './jwt/jwt.module';
+import { AuthModule } from './auth/auth.module';
+import { BcryptModule } from './bcrypt/bcrypt.module';
 
 @Module({
-  imports: [EnvConfigModule, DatabaseModule],
+  imports: [
+    EnvConfigModule,
+    DatabaseModule,
+    JwtModule,
+    AuthModule,
+    BcryptModule,
+  ],
 })
 export class AppModule {}
