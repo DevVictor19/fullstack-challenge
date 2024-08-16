83f38a6292bd4cb1e966ad466454bf766bcc2ee5
DevVictor19 <antoniovictor12@live.com>
2024-08-16 14:02:55 -0300

    chore(backend): add open api doc

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { HttpStatus, ValidationPipe } from '@nestjs/common';
 import { IEnvConfigProvider } from './env-config/env-config-provider.interface';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -13,6 +14,14 @@ async function bootstrap() {
       transform: true,
     }),
   );
+
+  const config = new DocumentBuilder()
+    .setTitle('Fullstack Challenge API')
+    .setDescription('Api de gerenciamento de clientes e seus contatos')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(app.get(IEnvConfigProvider).getServerPort());
 }
